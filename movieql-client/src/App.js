e26d71e7e00a9725014dae4f1ae3f258f9655827
jihyunglee <jhlee7125@gmail.com>
2023-07-24 10:11:24 +0900

    feat: route setup

--- movieql-client/src/App.js
+++ movieql-client/src/App.js
@@ -1,5 +1,14 @@
-function App() {
-  return <div className='App'></div>;
-}
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Movie from './routes/Movie';
+import Movies from './routes/Movies';
 
-export default App;
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Movies />} />
+        <Route path='/movies/:id' element={<Movie />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
