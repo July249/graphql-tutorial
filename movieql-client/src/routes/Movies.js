9d88c65b734a7c95b6dbccb6ccfcdc92ecc08d9c
jihyunglee <jhlee7125@gmail.com>
2023-07-26 17:32:52 +0900

    Fetch Data with bad way 💩

--- movieql-client/src/routes/Movies.js
+++ movieql-client/src/routes/Movies.js
@@ -1,3 +1,31 @@
+import { useApolloClient, gql } from '@apollo/client';
+import { useState, useEffect } from 'react';
+
 export default function Movies() {
-  return <div>This is a list of movies</div>;
+  const [movies, setMovies] = useState([]);
+  const client = useApolloClient();
+
+  // 아래의 작업은 fetch로 데이터를 가져오는 것과 같다. => It's suck!
+  useEffect(() => {
+    client
+      .query({
+        query: gql`
+          {
+            allMovies {
+              id
+              title
+            }
+          }
+        `,
+      })
+      .then((result) => setMovies(result.data.allMovies));
+  }, [client]);
+
+  return (
+    <div>
+      {movies.map((movie) => (
+        <li key={movie.id}>{movie.title}</li>
+      ))}
+    </div>
+  );
 }
