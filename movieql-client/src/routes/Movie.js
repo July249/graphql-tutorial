6961b650e97d6a408629f5286e90b92d234d349c
jihyunglee <jhlee7125@gmail.com>
2023-07-26 18:21:02 +0900

    Caching query results - useQuery Variables

--- movieql-client/src/routes/Movie.js
+++ movieql-client/src/routes/Movie.js
@@ -1,3 +1,41 @@
+import { useParams } from 'react-router-dom';
+import { gql, useQuery } from '@apollo/client';
+
+const GET_MOVIE = gql`
+  query getMovie($movieId: ID!) {
+    movie(id: $movieId) {
+      id
+      title
+      summary
+      language
+      userId {
+        id
+        nickname
+        email
+      }
+    }
+  }
+`;
+
 export default function Movie() {
-  return <div>This is movie details</div>;
+  const { id } = useParams();
+
+  const { data, loading, error } = useQuery(GET_MOVIE, {
+    variables: { movieId: id },
+  });
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  console.log(loading);
+
+  return (
+    <div>
+      <h1>영화 제목: {data.movie.title}</h1>
+      <h3>리뷰어: {data.movie.userId.nickname}</h3>
+      <span style={{ fontSize: 14 }}>영화 언어: {data.movie.language}</span>
+      <p>영화 설명: {data.movie.summary}</p>
+    </div>
+  );
 }
--- movieql-client/src/routes/Movies.js
+++ movieql-client/src/routes/Movies.js
@@ -1,6 +1,6 @@
+import { Link } from 'react-router-dom';
 import { gql, useQuery } from '@apollo/client';
 
-// "query getMovies"는 그냥 이름이다. 아무거나 쓰거나 안써도 된다. <= 프론트엔드에서 어떻게 쓸 건지 정해야 함
 const ALL_MOVIES = gql`
   query getMovies {
     allMovies {
@@ -22,7 +22,6 @@ const ALL_MOVIES = gql`
 
 export default function Movies() {
   const { data, loading, error } = useQuery(ALL_MOVIES);
-  // console.log(data);
 
   if (loading) {
     return <div>Loading...</div>;
@@ -37,7 +36,9 @@ export default function Movies() {
       <p>영화 제목 리스트 - 리뷰어 닉네임</p>
       {data.allMovies.map((movie) => (
         <li key={movie.id}>
-          영화제목: {movie.title} - 리뷰어: {movie.userId.nickname}
+          <Link to={`/movies/${movie.id}`}>
+            영화제목: {movie.title} - 리뷰어: {movie.userId.nickname}
+          </Link>
         </li>
       ))}
       <p>리뷰어 닉네임</p>
