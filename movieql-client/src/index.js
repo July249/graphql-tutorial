7c8033de1840b5f1c5b51addfac8c1184c9d167d
jihyunglee <jhlee7125@gmail.com>
2023-07-26 17:52:27 +0900

    Fetch Data with fancy way ✨

--- movieql-client/src/index.js
+++ movieql-client/src/index.js
@@ -2,13 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import client from './client';
-// Step 4. Connect your client to React
 import { ApolloProvider } from '@apollo/client';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    {/* ApolloProvider로 App을 감쌌기 때문에 어느 컴포넌트에서나 client에 접근이 가능하다. */}
     <ApolloProvider client={client}>
       <App />
     </ApolloProvider>
--- movieql-client/src/routes/Movies.js
+++ movieql-client/src/routes/Movies.js
@@ -1,30 +1,50 @@
-import { useApolloClient, gql } from '@apollo/client';
-import { useState, useEffect } from 'react';
+import { gql, useQuery } from '@apollo/client';
+
+// "query getMovies"는 그냥 이름이다. 아무거나 쓰거나 안써도 된다. <= 프론트엔드에서 어떻게 쓸 건지 정해야 함
+const ALL_MOVIES = gql`
+  query getMovies {
+    allMovies {
+      id
+      userId {
+        id
+        email
+        nickname
+      }
+      title
+    }
+    allUsers {
+      nickname
+      email
+      id
+    }
+  }
+`;
 
 export default function Movies() {
-  const [movies, setMovies] = useState([]);
-  const client = useApolloClient();
+  const { data, loading, error } = useQuery(ALL_MOVIES);
+  // console.log(data);
 
-  // 아래의 작업은 fetch로 데이터를 가져오는 것과 같다. => It's suck!
-  useEffect(() => {
-    client
-      .query({
-        query: gql`
-          {
-            allMovies {
-              id
-              title
-            }
-          }
-        `,
-      })
-      .then((result) => setMovies(result.data.allMovies));
-  }, [client]);
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>Something went wrong: {error.message}</div>;
+  }
 
   return (
     <div>
-      {movies.map((movie) => (
-        <li key={movie.id}>{movie.title}</li>
+      <p>영화 제목 리스트 - 리뷰어 닉네임</p>
+      {data.allMovies.map((movie) => (
+        <li key={movie.id}>
+          영화제목: {movie.title} - 리뷰어: {movie.userId.nickname}
+        </li>
+      ))}
+      <p>리뷰어 닉네임</p>
+      {data.allUsers.map((user) => (
+        <li key={user.id}>
+          {user.nickname} (email: {user.email})
+        </li>
       ))}
     </div>
   );
