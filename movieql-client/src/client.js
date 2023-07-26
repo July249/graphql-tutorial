657b379b225efbb86f2e8b5f8c929c057882cec4
jihyunglee <jhlee7125@gmail.com>
2023-07-26 17:20:36 +0900

    Connect your client to React

--- movieql-client/src/client.js
+++ movieql-client/src/client.js
@@ -5,18 +5,4 @@ const client = new ApolloClient({
   cache: new InMemoryCache(),
 });
 
-// hook을 사용하지 않고 query 날리는 방법
-client
-  .query({
-    query: gql`
-      {
-        allMovies {
-          id
-          medium_cover_image
-        }
-      }
-    `,
-  })
-  .then((data) => console.log(data));
-
 export default client;
--- movieql-client/src/index.js
+++ movieql-client/src/index.js
@@ -2,10 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import client from './client';
+// Step 4. Connect your client to React
+import { ApolloProvider } from '@apollo/client';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>
 );
