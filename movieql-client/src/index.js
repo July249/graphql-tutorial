4462083acfd46d3ea9d1c9ff9e131780c93f05cd
jihyunglee <jhlee7125@gmail.com>
2023-07-26 17:22:30 +0900

    Connect your client to React

--- movieql-client/src/index.js
+++ movieql-client/src/index.js
@@ -8,6 +8,7 @@ import { ApolloProvider } from '@apollo/client';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    {/* ApolloProvider로 App을 감쌌기 때문에 어느 컴포넌트에서나 client에 접근이 가능하다. */}
     <ApolloProvider client={client}>
       <App />
     </ApolloProvider>
